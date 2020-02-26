217034a19499fe5c0c5a6cd2d1e8071891d76d6a
Usman Haidar <usmanhaidar56@gmail.com>
2020-02-26 11:37:48 +1100

    name changes

--- gitRep/scraper/scraper/settings.py
+++ gitRep/scraper/scraper/settings.py
@@ -9,10 +9,10 @@
 #     https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
 #     https://docs.scrapy.org/en/latest/topics/spider-middleware.html
 
-BOT_NAME = 'tutorial'
+BOT_NAME = 'scraper'
 
-SPIDER_MODULES = ['tutorial.spiders']
-NEWSPIDER_MODULE = 'tutorial.spiders'
+SPIDER_MODULES = ['scraper.spiders']
+NEWSPIDER_MODULE = 'scraper.spiders'
 
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
